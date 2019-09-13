dd77ba49f596e6d51203bea4512895d08b1b1739
ChickenHawkXP <47929127+ChickenHawkXP@users.noreply.github.com>
2019-09-13 14:52:09 -0400

    V.2 Search functions

--- main.py
+++ main.py
@@ -8,6 +8,7 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QAction, QTableWidget,QTableWidgetItem,QVBoxLayout
 import mysql.connector
 import json
 table = 'information'
@@ -83,13 +84,9 @@ class Ui_MainWindow(object):
         font.setPointSize(16)
         self.label_7.setFont(font)
         self.label_7.setObjectName("label_7")
-        self.label_8 = QtWidgets.QLabel(self.centralwidget)
-        self.label_8.setGeometry(QtCore.QRect(350, 10, 111, 31))
         font = QtGui.QFont()
         font.setFamily("Comic Sans MS")
         font.setPointSize(16)
-        self.label_8.setFont(font)
-        self.label_8.setObjectName("label_8")
         self.label_9 = QtWidgets.QLabel(self.centralwidget)
         self.label_9.setGeometry(QtCore.QRect(350, 110, 111, 31))
         font = QtGui.QFont()
@@ -97,43 +94,33 @@ class Ui_MainWindow(object):
         font.setPointSize(16)
         self.label_9.setFont(font)
         self.label_9.setObjectName("label_9")
-        self.label_10 = QtWidgets.QLabel(self.centralwidget)
-        self.label_10.setGeometry(QtCore.QRect(350, 200, 111, 31))
         font = QtGui.QFont()
         font.setFamily("Comic Sans MS")
         font.setPointSize(16)
-        self.label_10.setFont(font)
-        self.label_10.setObjectName("label_10")
-        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(20, 60, 113, 20))
-        self.lineEdit.setObjectName("lineEdit")
-        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_2.setGeometry(QtCore.QRect(20, 160, 113, 20))
-        self.lineEdit_2.setObjectName("lineEdit_2")
-        self.lineEdit_3 = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_3.setGeometry(QtCore.QRect(20, 250, 113, 20))
-        self.lineEdit_3.setObjectName("lineEdit_3")
-        self.lineEdit_4 = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_4.setGeometry(QtCore.QRect(20, 340, 113, 20))
-        self.lineEdit_4.setObjectName("lineEdit_4")
-        self.lineEdit_5 = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_5.setGeometry(QtCore.QRect(200, 250, 113, 20))
-        self.lineEdit_5.setObjectName("lineEdit_5")
-        self.lineEdit_6 = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_6.setGeometry(QtCore.QRect(200, 160, 113, 20))
-        self.lineEdit_6.setObjectName("lineEdit_6")
-        self.lineEdit_7 = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_7.setGeometry(QtCore.QRect(200, 60, 113, 20))
-        self.lineEdit_7.setObjectName("lineEdit_7")
-        self.lineEdit_8 = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_8.setGeometry(QtCore.QRect(350, 250, 113, 20))
-        self.lineEdit_8.setObjectName("lineEdit_8")
-        self.lineEdit_9 = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_9.setGeometry(QtCore.QRect(350, 160, 113, 20))
-        self.lineEdit_9.setObjectName("lineEdit_9")
-        self.lineEdit_10 = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_10.setGeometry(QtCore.QRect(350, 60, 113, 20))
-        self.lineEdit_10.setObjectName("lineEdit_10")
+        self.name = QtWidgets.QLineEdit(self.centralwidget)
+        self.name.setGeometry(QtCore.QRect(20, 60, 113, 20))
+        self.name.setObjectName("name")
+        self.lastname = QtWidgets.QLineEdit(self.centralwidget)
+        self.lastname.setGeometry(QtCore.QRect(20, 160, 113, 20))
+        self.lastname.setObjectName("lastname")
+        self.email = QtWidgets.QLineEdit(self.centralwidget)
+        self.email.setGeometry(QtCore.QRect(20, 250, 113, 20))
+        self.email.setObjectName("email")
+        self.dob = QtWidgets.QLineEdit(self.centralwidget)
+        self.dob.setGeometry(QtCore.QRect(20, 340, 113, 20))
+        self.dob.setObjectName("dob")
+        self.major = QtWidgets.QLineEdit(self.centralwidget)
+        self.major.setGeometry(QtCore.QRect(200, 250, 113, 20))
+        self.major.setObjectName("major")
+        self.sex = QtWidgets.QLineEdit(self.centralwidget)
+        self.sex.setGeometry(QtCore.QRect(200, 160, 113, 20))
+        self.sex.setObjectName("sex")
+        self.address = QtWidgets.QLineEdit(self.centralwidget)
+        self.address.setGeometry(QtCore.QRect(200, 60, 113, 20))
+        self.address.setObjectName("address")
+        self.phone = QtWidgets.QLineEdit(self.centralwidget)
+        self.phone.setGeometry(QtCore.QRect(350, 160, 113, 20))
+        self.phone.setObjectName("phone")
         self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_3.setGeometry(QtCore.QRect(210, 320, 221, 51))
         font = QtGui.QFont()
@@ -144,7 +131,7 @@ class Ui_MainWindow(object):
         self.lists = QtWidgets.QTableWidget(self.centralwidget)
         self.lists.setGeometry(QtCore.QRect(510, 40, 331, 221))
         self.lists.setObjectName("QTableWidget")
-        self.lists.setRowCount(1)
+        self.lists.setRowCount(10)
         self.lists.setColumnCount(10)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
@@ -156,15 +143,88 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.pushButton_3.clicked.connect(self.search)
+        '''
+        self.name.textChanged.connect(self.search)
+        self.lastname.textChanged.connect(self.search)
+        self.email.textChanged.connect(self.search)
+        self.dob.textChanged.connect(self.search)
+        self.major.textChanged.connect(self.search)
+        self.sex.textChanged.connect(self.search)
+        self.address.textChanged.connect(self.search)
+        self.height.textChanged.connect(self.search)
+        self.phone.textChanged.connect(self.search)
+        self.pets.textChanged.connect(self.search)
+        '''
 
-    def loadDB(self,db):
-        self.lists.setItem(1,1,QtWidgets.QTableWidgetItem("First Name"))
+    def initDB(self,db):
+        self.lists.setItem(0,0,QTableWidgetItem("Name"))
+        self.lists.setItem(0,1,QTableWidgetItem("Last Name"))
+        self.lists.setItem(0,2,QTableWidgetItem("Date of Birth"))
+        self.lists.setItem(0,3,QTableWidgetItem("Email"))
+        self.lists.setItem(0,4,QTableWidgetItem("Address"))
+        self.lists.setItem(0,5,QTableWidgetItem("Phone"))
+        self.lists.setItem(0,6,QTableWidgetItem("Sex"))
+        self.lists.setItem(0,7,QTableWidgetItem("Major"))
+
         db.execute("SELECT * FROM %s" %table)
-        rowcount = self.lists.rowCount()
-        row = 1
+        row = 0
         for i in db:
-            self.lists.insertRow(rowcount)
-            print(i)
+            row += 1
+            col = 0
+            for x in range(10):
+                self.lists.setItem(row,col,QTableWidgetItem(i[x]))
+                print(i[x])
+                col +=1
+
+    def search(self):
+        self.lists.clear()
+        name = self.name.text()
+        lname = self.lastname.text()
+        email = self.email.text()
+        dob = self.dob.text()
+        major = self.major.text()
+        sex = self.sex.text()
+        address = self.address.text()
+        phone = self.phone.text()
+
+        if name == "":
+            name = '%%%%'
+        if lname == "":
+            lname = '%%%%'
+        if email == "":
+            email = '%%%%'
+        if dob == "":
+            dob = '%%%%'
+        if major == "":
+            major = '%%%%'
+        if sex == "":
+            sex = '%%%%'
+        if address == "":
+            address = '%%%%'
+        if phone == "":
+            phone = '%%%%'
+        cursor.execute("""SELECT * FROM %s
+                        WHERE `FNAME` LIKE '%s%%'
+                        AND `LNAME` LIKE '%s%%'
+                        AND `DOB` LIKE '%s%%'
+                        AND `EMAIL` LIKE '%s%%'
+                        AND `ADDRESS` LIKE '%s%%'
+                        AND `PHONE` LIKE '%s%%'
+                        AND `SEX` LIKE '%s%%'
+                        AND `MAJOR` LIKE '%s%%'
+                        """ %(table,name,lname,dob,email,address,phone,sex,major))
+        row = -1
+        try:
+            for i in cursor:
+                row += 1
+                col = 0
+                for x in range(10):
+                    self.lists.setItem(row,col,QTableWidgetItem(i[x]))
+                    col +=1
+        except:
+            pass
+
         
 
     def retranslateUi(self, MainWindow):
@@ -179,9 +239,7 @@ class Ui_MainWindow(object):
         self.label_5.setText(_translate("MainWindow", "Address"))
         self.label_6.setText(_translate("MainWindow", "Sex"))
         self.label_7.setText(_translate("MainWindow", "Major"))
-        self.label_8.setText(_translate("MainWindow", "# of Pets"))
         self.label_9.setText(_translate("MainWindow", "Phone"))
-        self.label_10.setText(_translate("MainWindow", "Height"))
         self.pushButton_3.setText(_translate("MainWindow", "Search"))
 
 if __name__ == "__main__":
@@ -190,6 +248,6 @@ if __name__ == "__main__":
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
-    ui.loadDB(cursor)
+    ui.initDB(cursor)
     MainWindow.show()
     sys.exit(app.exec_())
